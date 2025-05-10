9d76de03d949f6ae05f2f555d7e81b1284a185da
katayama8000 <tattu.0310@gmail.com>
2025-05-10 14:08:36 +0900

    Revert "Qrcode (#230)"
    This reverts commit 77f7a03c7f4df4a376ec938ecfb19b511edb620c.

--- generate_types.sh
+++ generate_types.sh
@@ -4,9 +4,15 @@ source ./.env
 # Start generating TypeScript types for your Supabase schema
 echo "Generating TypeScript types for your Supabase schema..."
 
+# Ensure the PROJECT_REF environment variable is set
+if [ -z $PROJECT_REF ]; then
+  echo "PROJECT_REF environment variable is not set in .env file" >&2
+  exit 1
+fi
+
 # Generate TypeScript types for your Supabase schema
 npx supabase gen types --lang=typescript \
-  --project-id jqovxmsueffhddmyqcew \
+  --project-id $PROJECT_REF \
   --schema public > types/supabase.ts
 
 # Check if the generation was successful
