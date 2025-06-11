8f526dea26a3d093274d874fdeb2eee1ccf93c51
katayama8000 <tattu.0310@gmail.com>
2025-06-11 10:55:55 +0900

    Remove app json and build dynamic config (#264)
    * Add environment fields for production, development, and local builds in eas.json
    
    * Add development guidelines for JavaScript/TypeScript and React
    
    * Refactor invoice type definitions for better clarity and consistency in payments handling
    
    * Refactor app configuration by removing app.json and updating app.config.ts with enhanced structure and additional fields
    
    * Remove app development guidelines document
    
    * Fix formatting issues in app.config.ts by adding missing commas for better readability and consistency

--- app.config.ts
+++ app.config.ts
@@ -31,19 +31,64 @@ const appEnv = (process.env.APP_ENV ?? "local") as AppEnv;
 if (!isAppEnv(appEnv)) throw new Error(`unsupported APP_ENV: ${appEnv}`);
 
 export default ({ config }: ConfigContext): ExpoConfig => {
-  const {  googleServicesJson, name, package: packageName } = envConfigs[appEnv];
+  const { bundleId, googleServicesJson, package: packageName, name: appName } = envConfigs[appEnv];
+
   return {
-    ...config,
+    name: appName,
     slug: "household-account-book",
-    name,
-    extra: {
-      ...config.extra,
+    orientation: "portrait",
+    icon: "./assets/images/moufu_n_ikura.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    version: "2.3.2",
+    splash: {
+      image: "./assets/images/moufu.png",
+      resizeMode: "contain",
+      backgroundColor: "#336666",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: bundleId,
     },
     android: {
-      ...config.android,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/moufu_n_ikura.png",
+        backgroundColor: "#336666",
+      },
       package: packageName,
-      // FIXME: get GOOGLE_SERVICES_JSON from expo secrets
+      versionCode: 23,
       googleServicesFile: googleServicesJson,
     },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/moufu.png",
+    },
+    plugins: [
+      "expo-router",
+      "expo-font",
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/images/ikura.jpg",
+          color: "#336666",
+          defaultChannel: "default",
+          sounds: ["./assets/sounds/cat.wav"],
+          enableBackgroundRemoteNotifications: false,
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "018a6711-ac8c-41b8-830c-279089162afa",
+      },
+    },
+    owner: "katayama9000",
   };
 };
