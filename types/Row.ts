43ed33fc9b51092983018210aa8557d41e0282fd
katayama8000 <tattu.0310@gmail.com>
2025-06-18 16:22:55 +0900

    Add Subscription type for couple_subscriptions in Row.ts

--- types/Row.ts
+++ types/Row.ts
@@ -4,3 +4,4 @@ export type Couple = Tables<"couples">;
 export type Payment = Tables<"payments">;
 export type Invoice = Tables<"monthly_invoices">;
 export type User = Tables<"users">;
+export type Subscription = Tables<"couple_subscriptions">;
