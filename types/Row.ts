b457a4a333dfbe3669133f8f0aacd18335c0e4f8
katayama8000 <tattu.0310@gmail.com>
2025-06-21 21:25:20 +0900

    Remove unused BillingCycle type from Row.ts

--- types/Row.ts
+++ types/Row.ts
@@ -5,6 +5,3 @@ export type Payment = Tables<"payments">;
 export type Invoice = Tables<"monthly_invoices">;
 export type User = Tables<"users">;
 export type Subscription = Tables<"couple_subscriptions">;
-
-// サブスクリプション関連の型
-export type BillingCycle = Subscription["billing_cycle"];
