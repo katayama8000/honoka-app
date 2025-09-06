f809cc69ea02158b3ab4705de40bef7ac8c17356
katayama8000 <tattu.0310@gmail.com>
2025-09-06 11:30:41 +0900

    Fix primary color value in Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -23,7 +23,7 @@ export const Colors = {
     tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
-  primary: "#7f20ddff",
+  primary: "#5703c4ff",
   secondary: "#f44336",
   white: "#fff",
   black: "#000",
