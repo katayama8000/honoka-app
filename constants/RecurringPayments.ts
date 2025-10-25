0487f231de574958c6b0270ff47107e53a468222
okutyann <62106514+okutyann@users.noreply.github.com>
2025-10-25 12:48:02 +0900

    Update RecurringPayments.ts (#336)

--- constants/RecurringPayments.ts
+++ constants/RecurringPayments.ts
@@ -2,9 +2,9 @@ export const RecurringPayments = () => {
   const recurringPayments = [
     { amount: 2000, item: "ガソリン", memo: "自動追加" },
     {
-      amount: 34310,
+      amount: 26809,
       item: "家賃",
-      memo: `自動追加 (68450+170)/2`,
+      memo: "自動追加 (68450+170-15000)/2 \n 振り込み手数料：170 家賃手当：15,000",
     },
   ] as const;
 
